5a06f346307f4667e981411905ddd0c2049fbb6c
RobertJN64 <robertjnies@gmail.com>
2026-04-21 20:27:03 -0400

    csr updates

--- data_postprocessing/controller_state_reconstruction.cpp
+++ data_postprocessing/controller_state_reconstruction.cpp
@@ -6,24 +6,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// struct IMU_Calib {
-//   double gyro_bias[3];
-//   double accel_correction_bias[3];
-//   double accel_correction_gain[3];
-// };
+struct IMU_Calib {
+  double gyro_bias[3];
+  double accel_correction_bias[3];
+  double accel_correction_gain[3];
+};
 
-// struct MAG_Calib {
-//   double hard_x;
-//   double hard_y;
-//   double hard_z;
-//   double soft[9];
-// };
+struct Mag_Calib {
+  float hard_x;
+  float hard_y;
+  float hard_z;
+  float soft[3][3];
+};
 
 flight_packet_t fp; // allows this to persist across calls
 float last_time;
 float this_time;
 Controller_Input ci;
 traj_point_pos *traj;
+Mag_Calib mcal;
+IMU_Calib ical;
 
 bool parse_log_entry(FILE *compressed_bin, FILE *reconstructed_bin) {
   uint8_t entry_type;
@@ -32,21 +34,22 @@ bool parse_log_entry(FILE *compressed_bin, FILE *reconstructed_bin) {
     return false;
   }
 
-  printf("Read log entry.\n");
+  // printf("Read log entry.\n");
 
   fp.GND_flag = false;
   fp.flight_armed = true;
 
   switch (entry_type) {
 
-  ENTRY_TRAJECTORY:
+  case ENTRY_TRAJECTORY: {
     uint32_t len;
     fread(&len, sizeof(uint32_t), 1, compressed_bin);
     traj = (traj_point_pos *)malloc(sizeof(traj_point_pos) * len);
     fread(traj, sizeof(traj_point_pos), len, compressed_bin);
     break;
+  }
 
-  ENTRY_LOOP_STATE:
+  case ENTRY_LOOP_STATE: {
     LoopState ls;
     fread(&ls, sizeof(LoopState), 1, compressed_bin);
     this_time = ls.time;
@@ -62,8 +65,9 @@ bool parse_log_entry(FILE *compressed_bin, FILE *reconstructed_bin) {
     fp.target_pos_west = ci.target_pos_west;
     fp.target_pos_up = ci.target_pos_up;
     break;
+  }
 
-  ENTRY_SENSOR:
+  case ENTRY_SENSOR: {
     SensorEntry se;
     fread(&se, sizeof(SensorEntry), 1, compressed_bin);
 
@@ -90,8 +94,9 @@ bool parse_log_entry(FILE *compressed_bin, FILE *reconstructed_bin) {
     fp.mag_y = ci.mag_y;
     fp.mag_z = ci.mag_z;
     break;
+  }
 
-  ENTRY_GPS:
+  case ENTRY_GPS: {
     GpsEntry gps;
     fread(&gps, sizeof(GpsEntry), 1, compressed_bin);
 
@@ -112,8 +117,9 @@ bool parse_log_entry(FILE *compressed_bin, FILE *reconstructed_bin) {
     fp.gps_vel_west = ci.gps_vel_west;
     fp.gps_vel_up = ci.gps_vel_up;
     break;
+  }
 
-  ENTRY_CONTROLLER_OUT:
+  case ENTRY_CONTROLLER_OUT: {
     Controller_Output logged_co;
     fread(&logged_co, sizeof(Controller_Output), 1, compressed_bin);
 
@@ -152,24 +158,35 @@ bool parse_log_entry(FILE *compressed_bin, FILE *reconstructed_bin) {
 
     fwrite(&fp, sizeof(fp), 1, reconstructed_bin);
     break;
+  }
 
-  ENTRY_CALIB: // TODO - calibration
-    break;     // don't care about calibration for now
+  case ENTRY_CALIB: { // TODO - use this data
+    fread(&ical, sizeof(IMU_Calib), 1, compressed_bin);
+    fread(&mcal, sizeof(Mag_Calib), 1, compressed_bin);
+    break;
+  }
 
-  ENTRY_X_EST: // TODO - check x_est for drift
+  case ENTRY_X_EST: { // TODO - check x_est for drift
     fread((uint8_t *)(ControllerAndEstimator::x_est.data()), sizeof(ControllerAndEstimator::x_est(0)), ControllerAndEstimator::x_est.size(), compressed_bin);
     break;
+  }
 
-  ENTRY_FLIGHT_P:
+  case ENTRY_FLIGHT_P: {
     fread((uint8_t *)(ControllerAndEstimator::Flight_P.data()), sizeof(ControllerAndEstimator::Flight_P(0)), ControllerAndEstimator::Flight_P.size(), compressed_bin);
     break;
+  }
+
+  default: {
+    printf("Error - invalid entry type.\n");
+    break;
+  }
   }
 
   return true;
 }
 
 int main() {
-  FILE *compressed_bin = fopen("flash_dump.bin", "rb");
+  FILE *compressed_bin = fopen("gimbal_ema_test.bin", "rb");
   if (compressed_bin == NULL) {
     printf("Failed to open compressed log dump.");
     return EXIT_FAILURE;
@@ -186,7 +203,7 @@ int main() {
   ControllerAndEstimator::init_controller_and_estimator_constants();
 
   while (parse_log_entry(compressed_bin, reconstructed_bin)) {
-  };
+    };
 
   printf("Finished reading log.");
   fclose(compressed_bin);
